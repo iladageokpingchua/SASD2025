72d12c37f1fcff0deda01c8019b1ee12f40a1735
Pakorn <pakornkiat.jims@gmail.com>
2026-01-09 15:39:25 +0700

    Add project files.

---
+++ Restaurant/WaiterForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Restaurant
+{
+    partial class WaiterForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBox1 = new TextBox();
+            Send = new Button();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(122, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(406, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // Send
+            // 
+            Send.Location = new Point(548, 48);
+            Send.Name = "Send";
+            Send.Size = new Size(81, 25);
+            Send.TabIndex = 2;
+            Send.Text = "Send";
+            Send.UseVisualStyleBackColor = true;
+            // 
+            // WaiterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Send);
+            Controls.Add(textBox1);
+            Name = "WaiterForm";
+            Text = "Waiter Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBox1;
+        private Button Send;
+    }
+}
